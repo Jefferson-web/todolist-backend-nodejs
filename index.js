4c593bef7972fd979e3f8c8571f4a0cb089212fe
Jefferson <jeffer_16_99@hotmail.com>
2021-07-10 23:25:35 -0500

    Ruta por defecto

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ connectDb();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Routes
-app.get('/', async function (req, res) {
+app.get('/Task', async function (req, res) {
     await Task.find({}, function (err, docs) {
         res.status(200).json({
             tasks: docs
@@ -19,11 +19,17 @@ app.get('/', async function (req, res) {
     });
 });
 
-app.get('/states', async function (req, res) {
+app.get('/State', async function (req, res) {
     await State.find({}, function (err, docs) {
         res.status(200).json({
             states: docs
         })
+    });
+});
+
+app.get('/', function (req, res) {
+    res.status(200).json({
+        message: 'Ruta por defecto'
     });
 });
 
